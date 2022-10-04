7eda598bed54ad045a7829c7e6e35452f6d70dd4
rshinyanskiy <rshinyanskiy@odin.com>
2022-10-04 22:24:30 +0300

    Introduce User model

--- auth_service/auth/authentication/models.py
+++ auth_service/auth/authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
-# Create your models here.
+from model_utils import Choices
+
+
+class User(AbstractBaseUser):
+    ROLES = Choices('admin', 'accountant', 'manager', 'worker')
+
+    email = models.EmailField(null=False, unique=True)
+    name = models.CharField(max_length=128)
+    role = models.CharField(max_length=100, choices=ROLES, default=ROLES.worker)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
